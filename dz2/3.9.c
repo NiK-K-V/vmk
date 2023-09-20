88dc06823030bc5a1ae6faa6d1cf80ba46d76315
NiK-K-V <67245326+NiK-K-V@users.noreply.github.com>
2023-09-20 19:51:51 +0300

    Delete dz2/3.9.c

--- dz2/3.9.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-	//если написать int i, char c до for, то ошибки не будет, но так тоже работает
-	for(int i=0, char c=getchar();c!=' ' && c!='\n' && c!='\t' && c!=EOF;i++, c=getchar());
-	printf("%d \n", i);
-	return 0;	
-}
-
